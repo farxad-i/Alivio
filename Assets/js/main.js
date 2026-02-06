c1f40127a12a309671a08dfaea7ddedbef44ada6
Kayfun4ik <ibragimov.farxad@gmail.com>
2026-02-06 14:40:03 +0400

    Finished Project with basic js

---
+++ Assets/js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("listitem").addEventListener("click", function () {
+    document.querySelector(".toggler").checked = false;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("listitem1").addEventListener("click", function () {
+    document.querySelector(".toggler").checked = false;
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("listitem2").addEventListener("click", function () {
+    document.querySelector(".toggler").checked = false;
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("listitem3").addEventListener("click", function () {
+    document.querySelector(".toggler").checked = false;
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("listitem4").addEventListener("click", function () {
+    document.querySelector(".toggler").checked = false;
+  });
+});
